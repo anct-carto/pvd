1f0d048c0e76abbbe6cd6561b91229e8482b1098
HassChg <52163102+HassChg@users.noreply.github.com>
2021-01-25 10:43:36 +0100

    css improvements

--- src/app.js
+++ src/app.js
@@ -1,51 +1,11 @@
-let data_url = "https://docs.google.com/spreadsheets/d/1BHiW6bJky9cjkZDNz3bcBxrKqcZTq5YNswoNhqJGmYM/edit#gid=0"
+const loading = document.getElementById("loading");
+const data_url = "https://docs.google.com/spreadsheets/d/1BHiW6bJky9cjkZDNz3bcBxrKqcZTq5YNswoNhqJGmYM/edit#gid=0"
 let spreadsheet_res = [];
-const loading = document.getElementById("loading")
+let tab = JSON.parse(sessionStorage.getItem("session_data"));
 let page_status;
 
-function init() {
-    Tabletop.init({
-      key: data_url,
-      callback: fetchSpreadsheetData,
-      simpleSheet: true
-    })
-  };
 
-function fetchSpreadsheetData(res) {
-    fetch("data/geom_com2020.geojson")
-        .then(res => res.json())
-        .then(com_geom => {
-            com_geom = com_geom.features;            
-            // 1/ filtre
-            com_geom = com_geom.filter(e => {
-                if(res.filter(f => f.insee_com == e.properties.insee_com).length >0) {
-                    return e
-                }
-            });
-            // 2 jointure
-            com_geom.forEach(e => {
-                res.forEach(d => {
-                    if(e.properties.insee_com == d.insee_com) {
-                        for (var key of Object.keys(d)) {
-                            e.properties[key] = d[key]
-                        }
-                    }
-                })
-            });
-            // for(let i =0; i < 100; i++) {
-            //     spreadsheet_res.push(com_geom[i].properties)
-            // }
-            com_geom.forEach(e => spreadsheet_res.push(e.properties))
-            sessionStorage.setItem("session_data", JSON.stringify(spreadsheet_res))
-            loading.remove();
-            page_status = "loaded"
-            if(sessionStorage.getItem("session_data")) {
-                tab = JSON.parse(sessionStorage.getItem("session_data"))
-            }
-    });
-};
 
-init();
 
 // ****************************************************************************
 // ****************************************************************************
@@ -193,8 +153,8 @@ let cardTemplate = {
             <div class= "card-body">
                 <ul>
                     <li>Département : {{ pvd.lib_dep }} ({{ pvd.insee_dep }})</li>         
-                    <li>Région : {{ pvd.lib_reg }}</li>
-                    <li v-if="pvd.lib_epci">EPCI : {{ pvd.lib_epci }}</li>
+                    <li>Région : {{ pvd.lib_reg }}</li>         
+                    <li v-if="pvd.lib_epci">EPCI : {{ pvd.lib_epci }}</li>         
                     <li v-if="pvd.pop">Nombre d'habitants : {{ pvd.pop }}</li>         
                     <li v-if="pvd.nom_maire">Nom du Maire : {{ pvd.nom_maire }}</li>         
                     <li v-if="pvd.date_signature">Date de signature de la convention : {{ pvd.date_signature }}</li>         
@@ -235,7 +195,7 @@ let leafletSidebar = {
                     <text-intro v-if="!show"></text-intro>
                     <!--<search-group></search-group><br>-->
                     <card :pvd="cardContent" v-if="show"></card><br>
-                    <button id ="back-btn" type="button" class="btn btn-primary" v-if="show" @click="show=false">
+                    <button id="back-btn" type="button" class="btn btn-primary" v-if="show" @click="onClick">
                         <i class="fa fa-chevron-left"></i>
                         Retour à l'accueil
                     </button>
@@ -250,15 +210,15 @@ let leafletSidebar = {
                 </h2>
                 <img src="img/logo_anct.png" width="100%" style = 'padding-bottom: 5%; padding-top: 5%;'>
                 <p>
-                    <b>Source des données :</b>
+                    <b>Source et administration des données :</b>
                     ANCT, programme Petites villes de demain
                 </p>
                 <p>
-                    <b>Réalisation :</b>
+                    <b>Réalisation  et maintenance de l'outil :</b>
                     ANCT, <a href = 'https://cartotheque.anct.gouv.fr/cartes' target="_blank">Cartographes</a> du pôle Analyse & diagnostics territoriaux - 01/2021
                 </p>
-                <p>Technologies utilisées: LeafletJS, Bootstrap, VueJS, Vue2Leaflet, TurfJS, API Adresse / Base adresse nationale</p>
-                <p>Le code source de cet outil est consultable sur Github (accès direct par le bouton dédié sur la barre latérale).</p>
+                <p>Technologies utilisées: Leaflet, Bootstrap, VueJS</p>
+                <p>Le code source de cet outil est libre et consultable sur Github (accès via le bouton de la barre latérale).</p>
             </div>
         </div>
     </div>`,
@@ -271,13 +231,15 @@ let leafletSidebar = {
     data() {
         return {
             show:false,
-            cardContent:null
+            cardContent:null,
         }
     },
     watch: {
         fromParent() {
-            this.show = true;
             this.cardContent = this.fromParent;
+            if(this.fromParent) {
+                this.show = true;
+            }
         },
     },
     computed: {
@@ -285,9 +247,18 @@ let leafletSidebar = {
             // return this.fromParent.slice(0, this.nbResults)
         }
     },
+    methods: {
+        onClick() {
+            this.cardContent = '';
+            this.show = !this.show;
+            this.$emit("clearMap", true) // tell parent to remove clicked marker layer
+        }
+    },
 };
 
 let marker;
+let circle;
+
 
 
 // init vue-leaflet
@@ -296,7 +267,7 @@ let { LMap, LTileLayer, LControlZoom, LMarker, LCircleMarker, LGeoJson, LTooltip
 let leafletMap = {
     template: `
         <div>
-            <leaflet-sidebar ref="sidebar" :fromParent="cardContent"></leaflet-sidebar>
+            <leaflet-sidebar ref="sidebar" :fromParent="cardContent" @clearMap="removeClickedMarker()"></leaflet-sidebar>
             <div id="mapid"></div>
         </div>`,
     data() {
@@ -304,7 +275,6 @@ let leafletMap = {
             mapOptions: {
                 zoom: 6,
                 attribution: 'Fond cartographique &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy;, contributeurs <a href="http://openstreetmap.org">OpenStreetMap</a>',
-                // enableTooltip: true,
                 center: [46.413220, 1.219482],
                 zoomSnap: 0.5,
                 maxZoom:18,
@@ -329,9 +299,13 @@ let leafletMap = {
             clickedMarker:{
                 latlng:null,
                 tooltip:null,
-                tooltipOptions:{permanent:true, direction:"top", className:'leaflet-clicked-tooltip'},
+                tooltipOptions:{
+                    permanent:true, 
+                    direction:"top", 
+                    className:'leaflet-tooltip-clicked'
+                },
                 options: {
-                    radius:8,
+                    radius:10,
                     fillOpacity:1,
                     fillColor:"orange",
                     color:"white",
@@ -352,22 +326,15 @@ let leafletMap = {
                 }
             },
             cardContent:null,
+            layerGroup:null
         }
     },
     components: {
-        LMap,
-        LTileLayer,
-        LControlZoom,
-        LCircleMarker,
-        LMarker,
-        LGeoJson,
-        LTooltip,
-        LIcon,
         'leaflet-sidebar':leafletSidebar,
     },
     methods: {
         initMap() {
-            this.map = L.map('mapid', this.mapOptions);
+            this.map = L.map('mapid', this.mapOptions); // init map
             
             // zoom control, fullscreen & scale bar
             L.control.zoom({position: 'topright'}).addTo(this.map);
@@ -385,7 +352,6 @@ let leafletMap = {
                 container: "sidebar", 
                 position: "left" 
             }).addTo(this.map);
-
             this.sidebar = sidebar;
             
             // prevent drag over the sidebar and the legend
@@ -395,37 +361,55 @@ let leafletMap = {
             
         },
         onClick(i) {
-            this.cardContent = i;
+            lib_com = i.lib_com;
+            let layer = L.layerGroup({}).addTo(this.map);
+            this.layerGroup = layer;
             if(!marker) {
-                marker = new L.marker([i.latitude, i.longitude]).addTo(this.map)
+                marker = new L.marker([i.latitude, i.longitude]);
+                circle = new L.circleMarker([i.latitude, i.longitude], this.clickedMarker.options).addTo(this.map)
+                marker.bindTooltip(lib_com, this.clickedMarker.tooltipOptions);
             } else {
                 marker.setLatLng([i.latitude, i.longitude])
-            }
+                marker.setTooltipContent(lib_com)
+                circle.setLatLng([i.latitude, i.longitude])
+            };
+            layer.addLayer(marker);
+            layer.addLayer(circle);
+            // send value to children
+            this.cardContent = i;
             this.sidebar.open("home");
-            this.clickedMarker.latlng = [i.latitude, i.longitude];
-            this.clickedMarker.tooltip = i.lib_com;
+        },
+        removeClickedMarker() {
+            this.cardContent = '';
+            this.map.removeLayer(this.layerGroup);
+            this.map.setView(this.mapOptions.center, this.mapOptions.zoom)
         },
         checkPageStatus() {
             if(page_status == undefined) {
-                window.setTimeout(this.checkPageStatus,100);
+                window.setTimeout(this.checkPageStatus,5);
             } else {
-                this.sidebar.open('home');
-
                 // clusters
-                var markers = L.markerClusterGroup();
-                for(let i =0; i < spreadsheet_res.length; i++) {
+                let markers = L.markerClusterGroup();
+                // ajout données
+                for(let i=0; i<spreadsheet_res.length; i++) {
                     e = spreadsheet_res[i];
-                    let marker = L.circleMarker([e.latitude, e.longitude],this.circleMarkerOptions);
+                    let marker = L.circleMarker([e.latitude, e.longitude],this.circleMarkerOptions)
+                        .bindTooltip(e.lib_com, this.tooltipOptions)
+                            .on("mouseover", (e) => {
+                            e.target.setStyle(this.clickedMarker.options)
+                        }).on("mouseout",(e) => {
+                            e.target.setStyle(this.circleMarkerOptions)
+                        }).on("click", (e) => {
+                            this.onClick(e.sourceTarget.content)
+                        });
                     marker.content = e;
-                    marker.bindTooltip(e.lib_com, this.tooltipOptions);
                     markers.addLayer(marker);
                 }
                 this.map.addLayer(markers);
-
-                markers.on("click", (e) => {
-                    this.onClick(e.sourceTarget.content)
-                });
-            }
+                setTimeout(() => {
+                    this.sidebar.open('home');
+                }, 150);
+            };
         },
         loadDepGeom() {
             // compute dep
@@ -437,15 +421,62 @@ let leafletMap = {
                     new L.GeoJSON(res, this.geojson.options).addTo(this.map)
                 }
             })
+        },
+        init() {
+            Tabletop.init({
+              key: data_url,
+              callback: this.fetchSpreadsheetData,
+              simpleSheet: true
+            })
+        },
+        fetchSpreadsheetData(res) {
+            fetch("data/geom_com2020.geojson")
+                .then(res => res.json())
+                .then(com_geom => {
+                    com_geom = com_geom.features;            
+                    // 1/ filtre
+                    com_geom = com_geom.filter(e => {
+                        if(res.filter(f => f.insee_com == e.properties.insee_com).length >0) {
+                            return e
+                        }
+                    });
+                    // 2 jointure
+                    com_geom.forEach(e => {
+                        res.forEach(d => {
+                            if(e.properties.insee_com == d.insee_com) {
+                                for (var key of Object.keys(d)) {
+                                    e.properties[key] = d[key]
+                                }
+                            }
+                        })
+                    });
+                    // 3 tableau final
+                    com_geom.forEach(e => spreadsheet_res.push(e.properties))
+                    sessionStorage.setItem("session_data", JSON.stringify(spreadsheet_res))
+                    page_status = "loaded";
+                    loading.remove();
+            });
         }
+
     },
     mounted() {
-        this.initMap();
-        this.loadDepGeom();
-        init();
-        this.checkPageStatus();
-    },
+        console.log(this.$parent);
 
+        if(tab) {
+            spreadsheet_res = tab;
+            console.info("Loading from session storage");
+            setTimeout(() => {                
+                page_status = "loaded";
+                loading.remove();
+            }, 300);
+        } else {
+            this.init(); // load data
+            console.info("Loading from drive");
+        };
+        this.initMap(); // load map
+        this.loadDepGeom(); // load dep geojson
+        this.checkPageStatus(); // remove loading spinner
+    },
 }
 
 
@@ -457,35 +488,19 @@ let leafletMap = {
 // instance vue
 let vm = new Vue({
     el: '#app',
-    data() {
-        return {
-
-        }
-    },
     components: {
         'leaflet-map': leafletMap,
-        // 'search-group': search_group_template
+    },
+    methods: {
+
     },
 });
 
+
+// ****************************************************************************
+// ****************************************************************************
 // fonctions
 
-function onEachFeature(feature, layer) {
-    var v = this;
-    console.log(this);
-  
-    layer.on('mouseover', function (e) {
-        console.log("yes");
-        e.target.setStyle({
-            radius:15
-        });
-    });
-    layer.on('mouseout', function (e) {
-        e.target.setStyle({
-            radius:5
-        });
-    });
-};
 
 function preventDrag(div, map) {
     // Disable dragging when user's cursor enters the element
@@ -499,10 +514,28 @@ function preventDrag(div, map) {
     });
 };
 
-function joinData(df1,df2) {
-    let joined_data = df2.filter(e => {
-      return e.insee_com == df1.properties.insee_com
-    })
-    delete df1.properties.insee_com;
-    return joined_data;
-  };
+
+//   Canvas marker (test)
+
+                // let ciLayer = L.canvasIconLayer({}).addTo(this.map);
+                // icon = L.icon({
+                //     iconUrl: './img/icon_pvd.png',
+                //     iconSize: [15, 15],
+                //     iconAnchor: [10, 0]
+                // });
+                // markers = [];
+                // for(let i in spreadsheet_res) {
+                //     e = spreadsheet_res[i]
+                //     marker = L.marker([e.latitude, e.longitude],  {icon: icon});
+                //     marker.bindTooltip(e.lib_com, this.tooltipOptions)
+                //     marker.on("click", (e) => {
+                //         alert("oucou");
+                //     })
+                //     markers.push(marker)
+                // }
+                // ciLayer.addLayers(markers)
+                // ciLayer.addEventListener("click", e => {
+                //     console.log(e);
+                //     this.onClick(e.sourceTarget.content)
+
+                // })
